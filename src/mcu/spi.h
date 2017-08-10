a5633b424cc4faeb2d4d4ebca7737bd469c0d77d
Alex Subaric <deadmeu@gmail.com>
2017-08-11 00:39:21 +1000

    Updated files and added some test code to the main project file.

--- src/mcu/spi.h
+++ src/mcu/spi.h
@@ -1,6 +1,16 @@
-#include <stdio.h>
+/*
+ * Modified by Alex Subaric from work made by Peter Sutton.
+ */ 
+ 
+#ifndef SPI_H_
+#define SPI_H_
 
-int main(int argc, char* argv) {
-    printf("Ayy lmao");
-    return 0;
-}
+// Set up SPI communication as a master.
+// clockdivider should be one of 2,4,8,16,32,64,128
+void spi_setup_master(uint8_t clockdivider);
+
+// Send and receive an SPI byte. This function will take at least 8 
+// cyles of the divided clock
+uint8_t spi_send_byte(uint8_t byte);
+
+#endif /* SPI_H_ */
