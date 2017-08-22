805d823a2c7fce80d0fea6d340e855bcb3e7b931
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2017-08-22 12:15:59 +1000

    Can flash and fade leds, and can send ~38khz using software

--- src/mcu/ir.c
+++ src/mcu/ir.c
@@ -1,1 +1,109 @@
-#include "ir.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#ifndef F_CPU
+#define F_CPU 16000000L
+#endif
+
+void init_carrier(void);
+void enable_carrier(void);
+void IR_carrier(uint16_t IRTimeMicroSeconds);
+void send_code(uint32_t IRcode);
+void send_1(void);
+void send_0(void);
+
+/*
+ * A test code to send.
+ */
+uint32_t IRcode = 0b11000001110001111100000000111111;
+
+/*
+ * Each pulse is 562us long.
+ */
+uint16_t bitTime = 562;
+
+/*
+ * Main loop.
+ */
+int main(void) {
+	while(1) {
+		init_carrier();
+		send_code(IRcode);
+		_delay_ms(500);
+	}
+	return 0;
+}
+
+/*
+ * Set the data direction for the IR IO. 
+ */
+void init_carrier(void) {
+	DDRD = 0xFF;
+	PORTD = 0x00;
+}
+
+/*
+ * We're sending a 38kHz carrier wave.
+ */
+void enable_carrier(void) {
+	PORTD = 0xFF;
+	_delay_us(12);
+	PORTD = 0x00;
+	_delay_us(12);
+}
+
+/*
+ * Enable the carrier for a certain amount of microseconds.
+ */
+void IR_carrier(uint16_t IRTimeMicroSeconds) {
+	int i;
+	for (i = 0; i < IRTimeMicroSeconds/26; i++) {
+		enable_carrier();
+	}
+}
+
+/*
+ * Send a 0 bit on the carrier wave.
+ */
+void send_0(void) {
+/*
+ * The rest time for a 0 bit is 560us
+ */
+	IR_carrier(bitTime);
+	_delay_us(560);
+}
+
+/*
+ * Send a 1 bit on the carrier wave.
+ */
+void send_1(void) {
+/*
+ * The rest time for a 1 bit is 1690us
+ */
+ 	IR_carrier(bitTime);
+	_delay_us(1690);
+}
+
+void send_code(uint32_t IRcode) {
+/*
+ * The NEC protocol begins with a 9ms start burst.
+ */
+ 	IR_carrier(9000);
+/*
+ * 4.5ms silence follows.
+ */
+ 	_delay_us(4500);
+	int i;
+	for (i = 0; i < 32; i++) {
+		/*
+		 * Get the MSB
+		 */
+		if (IRcode & 0x80000000) {
+			send_1();
+		} else {
+			send_0();
+		}
+		IRcode <<= 1;
+	}
+}
---
+++ src/mcu/led.c
@@ -0,0 +1,80 @@
+/*
+* Light_WS2812 library example - RGB_blinky
+*
+* cycles one LED through red, green, blue
+*
+* This example is configured for a ATtiny85 with PLL clock fuse set and
+* the WS2812 string connected to PB4.
+*/
+
+#ifndef F_CPU
+#define F_CPU 16000000L
+#endif
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "light_ws2812.h"
+
+void blink(void);
+void fade(void);
+
+struct cRGB led[4];
+
+int main(void)
+{
+	while(1) {
+		blink();
+		fade();
+	}
+}
+
+void blink(void) {
+	int i;
+	for (i = 0; i < 2; i++) {
+		led[0].r=255;led[0].g=00;led[0].b=0;    // Write red to array
+		led[1].r=255;led[1].g=00;led[1].b=0;    // Write red to array
+		led[2].r=255;led[2].g=00;led[2].b=0;    // Write red to array
+		led[3].r=255;led[3].g=00;led[3].b=0;    // Write red to array
+		ws2812_setleds(led,4);
+		_delay_ms(500);                         // wait for 500ms.
+
+		led[0].r=0;led[0].g=255;led[0].b=0;			// green
+		led[1].r=0;led[1].g=255;led[1].b=0;			// green
+		led[2].r=0;led[2].g=255;led[2].b=0;			// green
+		led[3].r=0;led[3].g=255;led[3].b=0;			// green
+		ws2812_setleds(led,4);
+		_delay_ms(500);
+
+		led[0].r=0;led[0].g=00;led[0].b=255;		// blue
+		led[1].r=0;led[1].g=00;led[1].b=255;		// blue
+		led[2].r=0;led[2].g=00;led[2].b=255;		// blue
+		led[3].r=0;led[3].g=00;led[3].b=255;		// blue
+		ws2812_setleds(led,4);
+		_delay_ms(500);
+	}
+}
+
+void fade(void) {
+	int j;
+	int i;
+	for (j = 0; j < 4; j++) {
+		for (i = 0; i < 255; i++) {
+			led[0].r=i;led[0].g=i;led[0].b=i;   
+			led[1].r=i;led[1].g=i;led[1].b=i;  
+			led[2].r=i;led[2].g=i;led[2].b=i; 
+			led[3].r=i;led[3].g=i;led[3].b=i;
+			ws2812_setleds(led,4);
+			_delay_ms(2);
+		}
+		for (i = 255; i > 0; i--) {
+			led[0].r=i;led[0].g=i;led[0].b=i;   
+			led[1].r=i;led[1].g=i;led[1].b=i;  
+			led[2].r=i;led[2].g=i;led[2].b=i; 
+			led[3].r=i;led[3].g=i;led[3].b=i;
+			ws2812_setleds(led,4);
+			_delay_ms(2);
+		}
+		_delay_ms(100);
+	}
+}
