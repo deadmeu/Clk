84bee4e904b7bcab3a6f13400a7c1ab1f966313d
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2017-08-24 14:27:59 +1000

    Cleaned up ledarray

--- src/mcu/ledarray.c
+++ src/mcu/ledarray.c
@@ -10,19 +10,3 @@
 
 #define NUM_LEDS 4
 
-struct cRGB ledarray[NUM_LEDS];
-
-void ledarray_setup(void) {
-	// Setup the led array
-}
-
-void ledarray_update_pixel(uint8_t num, struct cRGB pixel) {
-	ledarray[num] = pixel;
-}
-
-void ledarray_clear(void) {
-	for (int i = 0; i < NUM_LEDS; i++) {
-		// Set the r, g, b values of each led to 0
-		//ledarray[i].r, ledarray[i].g, ledarray[i].b = 0;
-	}
-}
