85fe08f19b352e2dd640a90c55f5dcad5f19eb98
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2017-08-25 10:38:40 +1000

    moar colours

--- src/mcu/led_demo.c
+++ src/mcu/led_demo.c
@@ -1,16 +1,27 @@
 /*
- * Flashes RGB on 4 leds and fades from black to white.
- * Adapted from the light_ws2812 blinky example.
- * Written by Huy Nguyen.
- */
+* Light_WS2812 library example - RGB_blinky
+*
+* cycles one LED through red, green, blue
+*
+* This example is configured for a ATtiny85 with PLL clock fuse set and
+* the WS2812 string connected to PB4.
+*/
 
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "light_ws2812.h"
 
+#define BLINK_DELAY 200
+
 void blink(void);
 void fade(void);
+void red(void);
+void green(void);
+void blue(void);
+void yellow(void);
+void purple(void);
+void sky_blue(void);
 
 struct cRGB led[4];
 
@@ -18,56 +29,215 @@ int main(void)
 {
 	while(1) {
 		blink();
-		fade();
+		//fade();
 	}
 }
 
 void blink(void) {
-	int i;
-	for (i = 0; i < 2; i++) {
-		led[0].r=255;led[0].g=00;led[0].b=0;    // Write red to array
-		led[1].r=255;led[1].g=00;led[1].b=0;    // Write red to array
-		led[2].r=255;led[2].g=00;led[2].b=0;    // Write red to array
-		led[3].r=255;led[3].g=00;led[3].b=0;    // Write red to array
-		ws2812_setleds(led,4);
-		_delay_ms(500);                         // wait for 500ms.
+	//red();
+	//green();
+	blue();
+	//yellow();
+	//purple();
+	sky_blue();
+}
 
-		led[0].r=0;led[0].g=255;led[0].b=0;			// green
-		led[1].r=0;led[1].g=255;led[1].b=0;			// green
-		led[2].r=0;led[2].g=255;led[2].b=0;			// green
-		led[3].r=0;led[3].g=255;led[3].b=0;			// green
-		ws2812_setleds(led,4);
-		_delay_ms(500);
+void red(void) {
+	led[0].r=255;led[0].g=0;led[0].b=0;    // Write red to array
+	led[1].r=0;led[1].g=0;led[1].b=0;    // Write red to array
+	led[2].r=0;led[2].g=0;led[2].b=0;    // Write red to array
+	led[3].r=0;led[3].g=0;led[3].b=0;    // Write red to array
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);                         // wait for 500ms.
 
-		led[0].r=0;led[0].g=00;led[0].b=255;		// blue
-		led[1].r=0;led[1].g=00;led[1].b=255;		// blue
-		led[2].r=0;led[2].g=00;led[2].b=255;		// blue
-		led[3].r=0;led[3].g=00;led[3].b=255;		// blue
-		ws2812_setleds(led,4);
-		_delay_ms(500);
-	}
+	led[0].r=0;led[0].g=0;led[0].b=0;    // Write red to array
+	led[1].r=255;led[1].g=0;led[1].b=0;    // Write red to array
+	led[2].r=0;led[2].g=0;led[2].b=0;    // Write red to array
+	led[3].r=0;led[3].g=0;led[3].b=0;    // Write red to array
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);                         // wait for 500ms.
+
+	led[0].r=0;led[0].g=0;led[0].b=0;    // Write red to array
+	led[1].r=0;led[1].g=0;led[1].b=0;    // Write red to array
+	led[2].r=255;led[2].g=0;led[2].b=0;    // Write red to array
+	led[3].r=0;led[3].g=0;led[3].b=0;    // Write red to array
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);                         // wait for 500ms.
+
+	led[0].r=0;led[0].g=0;led[0].b=0;    // Write red to array
+	led[1].r=0;led[1].g=0;led[1].b=0;    // Write red to array
+	led[2].r=0;led[2].g=0;led[2].b=0;    // Write red to array
+	led[3].r=255;led[3].g=0;led[3].b=0;    // Write red to array
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);                         // wait for 500ms.
+}
+
+void green(void) {
+	led[0].r=0;led[0].g=255;led[0].b=0;			// green
+	led[1].r=0;led[1].g=0;led[1].b=0;			// green
+	led[2].r=0;led[2].g=0;led[2].b=0;			// green
+	led[3].r=0;led[3].g=0;led[3].b=0;			// green
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;			// green
+	led[1].r=0;led[1].g=255;led[1].b=0;			// green
+	led[2].r=0;led[2].g=0;led[2].b=0;			// green
+	led[3].r=0;led[3].g=0;led[3].b=0;			// green
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+	led[0].r=0;led[0].g=0;led[0].b=0;			// green
+	led[1].r=0;led[1].g=0;led[1].b=0;			// green
+	led[2].r=0;led[2].g=255;led[2].b=0;			// green
+	led[3].r=0;led[3].g=0;led[3].b=0;			// green
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+	led[0].r=0;led[0].g=0;led[0].b=0;			// green
+	led[1].r=0;led[1].g=0;led[1].b=0;			// green
+	led[2].r=0;led[2].g=0;led[2].b=0;			// green
+	led[3].r=0;led[3].g=255;led[3].b=0;			// green
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+}
+
+void blue(void) {
+	led[0].r=0;led[0].g=0;led[0].b=255;		// blue
+	led[1].r=0;led[1].g=0;led[1].b=0;		// blue
+	led[2].r=0;led[2].g=0;led[2].b=0;		// blue
+	led[3].r=0;led[3].g=0;led[3].b=0;		// blue
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;		// blue
+	led[1].r=0;led[1].g=0;led[1].b=255;		// blue
+	led[2].r=0;led[2].g=0;led[2].b=0;		// blue
+	led[3].r=0;led[3].g=0;led[3].b=0;		// blue
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;		// blue
+	led[1].r=0;led[1].g=0;led[1].b=0;		// blue
+	led[2].r=0;led[2].g=0;led[2].b=255;		// blue
+	led[3].r=0;led[3].g=0;led[3].b=0;		// blue
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;		// blue
+	led[1].r=0;led[1].g=0;led[1].b=0;		// blue
+	led[2].r=0;led[2].g=0;led[2].b=0;		// blue
+	led[3].r=0;led[3].g=0;led[3].b=255;		// blue
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+}
+
+void yellow(void) {
+	led[0].r=255;led[0].g=255;led[0].b=0;		
+	led[1].r=0;led[1].g=0;led[1].b=0;	
+	led[2].r=0;led[2].g=0;led[2].b=0;	
+	led[3].r=0;led[3].g=0;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=255;led[1].g=255;led[1].b=0;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=0;led[3].g=0;led[3].b=0;	
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;	
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=255;led[2].g=255;led[2].b=0;
+	led[3].r=0;led[3].g=0;led[3].b=0;	
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=255;led[3].g=255;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+}
+
+void purple(void) {
+	led[0].r=255;led[0].g=0;led[0].b=255;
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=0;led[3].g=0;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=255;led[1].g=0;led[1].b=255;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=0;led[3].g=0;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=255;led[2].g=0;led[2].b=255;
+	led[3].r=0;led[3].g=0;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=255;led[3].g=0;led[3].b=255;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+}
+
+void sky_blue(void) {
+	led[0].r=0;led[0].g=100;led[0].b=235;
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=0;led[3].g=0;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=0;led[1].g=100;led[1].b=235;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=0;led[3].g=0;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=0;led[2].g=100;led[2].b=235;
+	led[3].r=0;led[3].g=0;led[3].b=0;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
+	led[0].r=0;led[0].g=0;led[0].b=0;
+	led[1].r=0;led[1].g=0;led[1].b=0;
+	led[2].r=0;led[2].g=0;led[2].b=0;
+	led[3].r=0;led[3].g=100;led[3].b=235;
+	ws2812_setleds(led,4);
+	_delay_ms(BLINK_DELAY);
+
 }
 
 void fade(void) {
-	int j;
 	int i;
-	for (j = 0; j < 4; j++) {
-		for (i = 0; i < 255; i++) {
-			led[0].r=i;led[0].g=i;led[0].b=i;   
-			led[1].r=i;led[1].g=i;led[1].b=i;  
-			led[2].r=i;led[2].g=i;led[2].b=i; 
-			led[3].r=i;led[3].g=i;led[3].b=i;
-			ws2812_setleds(led,4);
-			_delay_ms(2);
-		}
-		for (i = 255; i > 0; i--) {
-			led[0].r=i;led[0].g=i;led[0].b=i;   
-			led[1].r=i;led[1].g=i;led[1].b=i;  
-			led[2].r=i;led[2].g=i;led[2].b=i; 
-			led[3].r=i;led[3].g=i;led[3].b=i;
-			ws2812_setleds(led,4);
-			_delay_ms(2);
-		}
-		_delay_ms(100);
+	for (i = 0; i < 255; i++) {
+		led[0].r=i;led[0].g=i;led[0].b=i;   
+		led[1].r=i;led[1].g=i;led[1].b=i;  
+		led[2].r=i;led[2].g=i;led[2].b=i; 
+		led[3].r=i;led[3].g=i;led[3].b=i;
+		ws2812_setleds(led,4);
+		_delay_ms(5);
 	}
+	for (i = 255; i > 0; i--) {
+		led[0].r=i;led[0].g=i;led[0].b=i;   
+		led[1].r=i;led[1].g=i;led[1].b=i;  
+		led[2].r=i;led[2].g=i;led[2].b=i; 
+		led[3].r=i;led[3].g=i;led[3].b=i;
+		ws2812_setleds(led,4);
+		_delay_ms(5);
+	}
+	_delay_ms(100);
 }
