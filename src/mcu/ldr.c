ab470b72537924fa99462f9fb027d8f5c5e1c269
deadmeu <deadmeu@gmail.com>
2017-10-04 10:53:57 +1000

    Fixed reference setting in ldr.c.

--- src/mcu/ldr.c
+++ src/mcu/ldr.c
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <avr/interrupt.h>
 #include "ldr.h"
+#include "clock.h"
 
 #define ADC_PIN            0    // PC0
 #define ADC_MIN          100
@@ -21,6 +22,9 @@
 static uint16_t adc_read(uint8_t pin);
 
 void init_ldr(void) {
+    // Set AVCC as reference
+    ADMUX = (1 << REFS0);
+
     // Enable ADC
     ADCSRA = _BV(ADEN);
 }
@@ -32,9 +36,18 @@ uint8_t get_ldr_opacity(void) {
     // Clamp the adc values between ADC_MIN and ADC_MAX
     result = (adc_value > ADC_MAX) ? ADC_MAX : adc_value;
     result = (adc_value < ADC_MIN) ? ADC_MIN : adc_value;
-
+	
     // Convert result from ADC value range to opacity value range
     result = (((result - ADC_MIN) * OPACITY_RANGE) / ADC_RANGE) + MIN_OPACITY;
+
+	
+	// if (result < 10) {
+	// 	set_meridiem_colours(1, 255,0,0);
+	// } else if (result >95 ) {
+	// 	set_meridiem_colours(1, 0,255,0);
+	// } else {
+	// 	set_meridiem_colours(1, 0, 0, 255);
+	// }
 
     return (uint8_t) result;
 }
