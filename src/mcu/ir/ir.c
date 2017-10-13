2e2ed48322e3a7e1f9fc5fb572cbab1e6cf17f63
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2017-10-13 12:07:32 +1000

    Updated serial functionality

--- src/mcu/ir/ir.c
+++ src/mcu/ir/ir.c
@@ -66,6 +66,11 @@ void init_ir(void) {
     UCSR0C = (1 << USBS0) | (1 << UCSZ00) | (1 << UCSZ01);
 
     /*
+     * Flush the USART data register.
+     */
+    USART_flush();
+
+    /*
      * Enable TX and RX and enable interrupt on receive.
      * Enable interrupt on serial read.
      */
@@ -77,8 +82,6 @@ void init_ir(void) {
     UBRR0H = (uint8_t) (UBRR_IR >> 8);
     UBRR0L = (uint8_t) (UBRR_IR);
 
-    size_marker = 0;
-    recv_marker = 0;
     updating_flag = 0;
     clear_receive_buffer();
 
@@ -247,4 +250,114 @@ ISR(USART_RX_vect) {
     if (usart_enabled()) {
         if (add_byte_to_buffer(UDR0) == BUFFER_FULL) disable_usart();
     }
+}
+
+/*****************************************************************************
+ * Functions to handle serial communication.
+ ****************************************************************************/
+
+void USART_init(uint32_t ubrr) {
+    cli();
+
+    /*
+     * Set the baud rate.
+     */
+    UBRR0H = (uint8_t) (ubrr >> 8);
+    UBRR0L = (uint8_t) (ubrr);
+
+    /*
+     * Flush the USART data register.
+     */
+    USART_flush();
+
+    /*
+     * Enable TX, RX and interrupt on receive complete.
+     */
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0);
+
+    /*
+     * Set frame format: 8 data, 2 stop bits.
+     */
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+
+    sei();
+}
+uint8_t USART_getc(void) {
+    while (!(UCSR0A & (1 << RXC0))) {
+    /*
+     * Wait for data to be received.
+     */
+    }
+    return UDR0;
+}
+
+void USART_putc(uint8_t data) {
+    while (!(UCSR0A & (1 << UDRE0))) {
+    /*
+     * Wait for the data register to empty.
+     */
+    }
+    UDR0 = data;
+}
+
+void USART_puts(char *data) {
+    while (!(UCSR0A & (1 << UDRE0))) {
+    /*
+     * Wait for the data register to empty.
+     */
+    }
+    for (int i = 0; i < strlen(data); i++) {
+        USART_putc(data[i]);
+    }
+    USART_putc('\r');
+    USART_putc('\n');
+}
+
+void USART_flush(void) {
+    char dummy;
+    while (UCSR0A & ( 1 << RXC0)) {
+        dummy = UDR0;
+    }
+}
+
+/*****************************************************************************
+ * Functions to handle the message buffer.
+ * The buffer is a matrix / 2d array of characters.
+ ****************************************************************************/
+
+void print_buffer(void) {
+    cursor_to_top_left();
+    for (int i = 0; i < BYTESTREAMS_RECV; i++) {
+        for (int j = 0; j < BYTESTREAMS_SIZE; j++) {
+            USART_putc(receive_buffer[i][j]);
+        }
+        USART_putc('\r');
+        USART_putc('\n');
+    }
+
+    /*
+     * Output newline to separate multiple printings of the buffer.
+     */
+    USART_putc('\r');
+    USART_putc('\n');
+}
+
+/*****************************************************************************
+ * Functions to handle terminal io.
+ ****************************************************************************/
+
+void initialise_terminal(void) {
+    /*
+     * Clear the screen.
+     */
+    USART_puts("\033[2J");
+
+    /*
+     * Hide the cursor.
+     */
+    USART_puts("\033[?25l");
+}
+
+void cursor_to_top_left(void) {
+    USART_puts("\033[H");
 }
