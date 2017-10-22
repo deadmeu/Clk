669cd10be37d6d7a279ba8969007f3176d9701a2
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2017-10-22 14:11:45 +1000

    Fulfilled license condition for rtc

--- src/mcu/rtc.c
+++ src/mcu/rtc.c
@@ -3,6 +3,10 @@
  *
  * Written by Huy Nguyen.
  * Modified by Alex Subaric.
+ * A copy of the i2c library used to implement this functionality can be
+ * found at
+ * http://homepage.hispeed.ch/peterfleury/doxygen/avr-gcc-libraries/group__\
+ * pfleury__ic2master.html
  */
 
 #include <avr/io.h>
